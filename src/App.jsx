d78b71ae63faa70e2d1af5756dc669d97f6ab000
moham60 <105382862+moham60@users.noreply.github.com>
2025-01-13 16:57:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
  
   const router = createBrowserRouter([
     {
-      path: 'reactApp1', element: <Layout />, children: [
+      path: '', element: <Layout />, children: [
         { path: '/', element: <Home homeImg={homeImg} /> }
         , {
           path: '/about', element: <About />
